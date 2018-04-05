98a3dcaf6014fd2dfb76cbd66d721ad38265346d
Michał Stróżyński <michal.strozynski@o2.pl>
2018-04-05 18:21:54 +0200

    More excel-like tab style and todo file

--- js/main.js
+++ js/main.js
@@ -50,10 +50,10 @@ function openTab(evt, tabName) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" tab-active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.currentTarget.className += " tab-active";
 }
